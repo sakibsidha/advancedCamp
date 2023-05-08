478ccb658fa4a45d4f628a02f24d3be69b79ef03
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2023-05-08 16:03:38 +0600

    Add files via upload

---
+++ c.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+void solve(){
+
+}
+
+int main(){
+    
+    //bitwise:
+    // int n=5;
+    // // cin>>n;
+    // int c{};
+    // for(int i=0;i<32;++i){
+    //     if((n&(1<<i))!=0) c++;
+    // }
+    // cout<<c;
+    // int n,x,flag{};
+    // cin>>n>>x;
+    // for(int i=0;i<32;++i){
+    //     if(i==x){
+    //         if((n&(1<<i))!=0){
+    //             flag=1;
+    //             break;
+    //         }
+    //     }
+    // }
+    // if(flag==1) cout<<"YES"<<endl;
+    // else cout<<"NO"<<endl;
+
+    //gcd:
+    // ll n=1000000000000;
+    // cout<<sqrt(n);
+    
+
+
+
+    return 0;
+}
